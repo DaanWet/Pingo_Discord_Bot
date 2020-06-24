4acce28c700332bd37f8e1f93416d0217d94d3f9
dwettinc <daan.wettinck@ugent.be>
2020-06-24 20:47:45 +0200

    Added Nickname command

---
+++ src/main/java/Commands/Nickname.java
@@ -0,0 +1,49 @@
+package Commands;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.ContextException;
+import net.dv8tion.jda.api.exceptions.HierarchyException;
+
+import java.net.Socket;
+import java.util.List;
+
+public class Nickname extends Command{
+
+    public Nickname(){
+        name = "nickname";
+        aliases = new String[]{"rename", "bijnaam"};
+    }
+
+
+    @Override
+    public void run(String[] args, GuildMessageReceivedEvent e) {
+        Message m = e.getMessage();
+        List<Member> mentionedmembers = m.getMentionedMembers();
+        if ( args.length >= 2 && ((mentionedmembers.size() == 1 && args[0].startsWith("<@!")) || e.getGuild().getMemberById(args[0]) != null )){
+            Member target = mentionedmembers.size() == 1 ? mentionedmembers.get(0) : e.getGuild().getMemberById(args[0]);
+            try {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 1; i < args.length; i++){
+                    System.out.println(i);
+                    System.out.println(args[i]);
+                    sb.append(args[i]).append(" ");
+                }
+                if(sb.toString().trim().length() <= 32){
+                    target.modifyNickname(sb.toString().trim()).queue();
+                } else {
+                    e.getChannel().sendMessage("Nickname too long").queue();
+                }
+            } catch (HierarchyException hexc){
+                e.getChannel().sendMessage("Sorry, I can't change the nickname of my master").queue();
+            }
+
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "Edits the nickname of a given person";
+    }
+}
