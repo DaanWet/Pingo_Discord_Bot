c7bf3d734d4dfeb47e7d55a1d850b157a3cfd452
Daan Wettinck <Daan.Wettinck@UGent.be>
2021-11-27 18:02:24 +0100

    Define record order

--- src/main/java/utils/DataHandler.java
+++ src/main/java/utils/DataHandler.java
@@ -50,7 +50,7 @@ public class DataHandler {
 
     public void createDatabase() {
         try (Connection conn = DriverManager.getConnection(JDBC_URL, properties);
-             PreparedStatement setuptable = conn.prepareStatement("CREATE TABLE IF NOT EXISTS Record (Type VARCHAR(50) NOT NULL PRIMARY KEY, IsInt BOOLEAN DEFAULT TRUE);" +
+             PreparedStatement setuptable = conn.prepareStatement("CREATE TABLE IF NOT EXISTS Record (ID INT, Name VARCHAR(50) NOT NULL PRIMARY KEY, IsInt BOOLEAN DEFAULT TRUE);" +
                                                                           "CREATE TABLE IF NOT EXISTS Member (UserId BIGINT NOT NULL, GuildId BIGINT NOT NULL, Credits INT DEFAULT 0, LastDaily TIMESTAMP, LastWeekly TIMESTAMP, Experience INT DEFAULT 0, CurrentStreak  INT DEFAULT 0, PRIMARY KEY(UserId, GuildId)); " +
                                                                           "CREATE TABLE IF NOT EXISTS RoleAssign (Name VARCHAR(255) NOT NULL, GuildId BIGINT NOT NULL, ChannelId BIGINT, MessageId BIGINT, Sorting VARCHAR(20) DEFAULT 'NONE', Compacting VARCHAR(20) DEFAULT 'NORMAL', Title VARCHAR(255), PRIMARY KEY(Name, GuildId));" +
                                                                           "CREATE TABLE IF NOT EXISTS Role (RoleId BIGINT NOT NULL, Name VARCHAR(255) NOT NULL, Emoji VARCHAR(255) NOT NULL, Type VARCHAR(255) NOT NULL, GuildId BIGINT NOT NULL, FOREIGN KEY (Type, GuildId) REFERENCES RoleAssign(Name, GuildId), PRIMARY KEY (Emoji, Type, GuildId));" +
@@ -58,13 +58,13 @@ public class DataHandler {
                                                                           "CREATE TABLE IF NOT EXISTS Setting (ID INT AUTO_INCREMENT PRIMARY KEY,  Name VARCHAR(50) NOT NULL, ValueType VARCHAR(10) NOT NULL, Type VARCHAR(50), Multiple BOOLEAN NOT NULL,  UNIQUE (Name, Type));" +
                                                                           "CREATE TABLE IF NOT EXISTS GuildSetting (GuildId BIGINT NOT NULL, ID INT NOT NULL, Value VARCHAR(255) NOT NULL, Type VARCHAR(50), FOREIGN KEY(ID) REFERENCES Setting(ID), PRIMARY KEY(GuildId, ID, Value));" +
                                                                           "CREATE TABLE IF NOT EXISTS Cooldown (GuildId BIGINT NOT NULL, UserId BIGINT NOT NULL, Setting INT NOT NULL, Time TIMESTAMP, PRIMARY KEY (GuildId, UserId, Setting), FOREIGN KEY (Setting) REFERENCES Setting(ID));" +
-                                                                          "INSERT IGNORE INTO Record VALUES ('highest_credits', TRUE);" +
-                                                                          "INSERT IGNORE INTO Record VALUES ('biggest_bj_win', TRUE);" +
-                                                                          "INSERT IGNORE INTO Record VALUES ('biggest_bj_loss', TRUE);" +
-                                                                          "INSERT IGNORE INTO Record VALUES ('bj_win_rate', FALSE);" +
-                                                                          "INSERT IGNORE INTO Record VALUES ('bj_games_played', TRUE);" +
-                                                                          "INSERT IGNORE INTO Record VALUES ('bj_win_streak', TRUE);" +
-                                                                          "INSERT IGNORE INTO Record VALUES ('bj_loss_streak', TRUE);"
+                                                                          "INSERT IGNORE INTO Record VALUES (1, 'highest_credits', TRUE) ON DUPLICATE KEY UPDATE ID = 1;" +
+                                                                          "INSERT IGNORE INTO Record VALUES (4, 'biggest_bj_win', TRUE) ON DUPLICATE KEY UPDATE ID = 4;" +
+                                                                          "INSERT IGNORE INTO Record VALUES (5, 'biggest_bj_loss', TRUE) ON DUPLICATE KEY UPDATE ID = 5;" +
+                                                                          "INSERT IGNORE INTO Record VALUES (3, 'bj_win_rate', FALSE) ON DUPLICATE KEY UPDATE ID = 2;" +
+                                                                          "INSERT IGNORE INTO Record VALUES (2, 'bj_games_played', TRUE) ON DUPLICATE KEY UPDATE ID = 3;" +
+                                                                          "INSERT IGNORE INTO Record VALUES (6, 'bj_win_streak', TRUE) ON DUPLICATE KEY UPDATE ID = 6;" +
+                                                                          "INSERT IGNORE INTO Record VALUES (7, 'bj_loss_streak', TRUE) ON DUPLICATE KEY UPDATE ID = 7;"
              )) {
             setuptable.executeUpdate();
             for (Setting s : Setting.values()) {
@@ -554,7 +554,7 @@ public class DataHandler {
     public ArrayList<RecordData> getRecords(long guildId, long userId) {
         ArrayList<RecordData> list = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(JDBC_URL, properties);
-             PreparedStatement stm = conn.prepareStatement("SELECT Name, Value, Link FROM UserRecord WHERE GuildId = ? AND UserId = ? ORDER BY Name DESC")) {
+             PreparedStatement stm = conn.prepareStatement("SELECT Name, Value, Link FROM UserRecord INNER JOIN Record USING(Name) WHERE GuildId = ? AND UserId = ? ORDER BY ID")) {
             stm.setLong(1, guildId);
             stm.setLong(2, userId);
             try (ResultSet set = stm.executeQuery()) {
@@ -572,7 +572,7 @@ public class DataHandler {
     public ArrayList<RecordData> getRecords(long guildId) {
         ArrayList<RecordData> list = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(JDBC_URL, properties);
-             PreparedStatement stm = conn.prepareStatement("SELECT a.UserId, a.Name, a.Value, a.Link FROM UserRecord a INNER JOIN (SELECT Name, MAX(Value) AS Max, GuildId FROM UserRecord WHERE GuildId = ? GROUP BY Name)  AS m ON a.Name = m.Name and a.Value = m.max and a.GuildId = m.GuildId ORDER BY a.Name DESC")) {
+             PreparedStatement stm = conn.prepareStatement("SELECT a.UserId, a.Name, a.Value, a.Link FROM (UserRecord a INNER JOIN (SELECT Name, MAX(Value) AS Max, GuildId FROM UserRecord WHERE GuildId = ? GROUP BY Name)  AS m ON a.Name = m.Name and a.Value = m.max and a.GuildId = m.GuildId) INNER JOIN Record r ON r.Name = a.Name ORDER BY ID")) {
             stm.setLong(1, guildId);
             try (ResultSet set = stm.executeQuery()) {
                 while (set.next()) {
@@ -588,7 +588,7 @@ public class DataHandler {
     public ArrayList<RecordData> getRecords(){
         ArrayList<RecordData> list = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(JDBC_URL, properties);
-             PreparedStatement stm = conn.prepareStatement("SELECT a.UserId, a.Name, a.Value, a.Link FROM UserRecord a INNER JOIN (SELECT Name, MAX(Value) AS Max FROM UserRecord GROUP BY Name)  AS m ON a.Name = m.Name and a.Value = m.max ORDER BY a.Name DESC")) {
+             PreparedStatement stm = conn.prepareStatement("SELECT a.UserId, a.Name, a.Value, a.Link FROM (UserRecord a INNER JOIN (SELECT Name, MAX(Value) AS Max FROM UserRecord GROUP BY Name)  AS m ON a.Name = m.Name and a.Value = m.max) INNER JOIN Record r ON r.Name = a.Name ORDER BY ID")) {
             try (ResultSet set = stm.executeQuery()) {
                 while (set.next()) {
                     list.add(new RecordData(set.getLong(1), set.getString(2), set.getDouble(3), set.getString(4)));
@@ -638,7 +638,7 @@ public class DataHandler {
     public ArrayList<String> getRecordTypes() {
         ArrayList<String> types = null;
         try (Connection conn = DriverManager.getConnection(JDBC_URL, properties);
-             PreparedStatement stm = conn.prepareStatement("SELECT Type FROM Record");
+             PreparedStatement stm = conn.prepareStatement("SELECT Name FROM Record");
              ResultSet set = stm.executeQuery()) {
             types = new ArrayList<>();
             while (set.next()) {
@@ -652,7 +652,7 @@ public class DataHandler {
 
     public boolean isInt(String record) {
         try (Connection conn = DriverManager.getConnection(JDBC_URL, properties);
-             PreparedStatement stm = conn.prepareStatement("SELECT IsInt FROM Record WHERE Type LIKE ?")) {
+             PreparedStatement stm = conn.prepareStatement("SELECT IsInt FROM Record WHERE Name LIKE ?")) {
             stm.setString(1, record);
             try (ResultSet set = stm.executeQuery()) {
                 if (set.next()) {
@@ -696,6 +696,8 @@ public class DataHandler {
             stm.setInt(7, Math.abs(value));
             stm.setString(8, link);
             stm.setInt(9, Math.abs(value));
+            stm.setString(10, link);
+            stm.setInt(11, Math.abs(value));
             stm.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
